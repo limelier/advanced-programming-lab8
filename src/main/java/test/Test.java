9819efb5a6773c867e2cd6feb447bc8ba873e62f
limelier <tudor.iacobescu.1@gmail.com>
2020-04-12 23:03:27 +0300

    Added Faker integration to the tests.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,5 +1,6 @@
 package test;
 
+import com.github.javafaker.Faker;
 import controllers.AlbumController;
 import controllers.ArtistController;
 import controllers.ChartController;
@@ -13,7 +14,9 @@ import org.bson.types.ObjectId;
 import util.ChartUtil;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 public class Test {
     public static void main(String[] args) throws Exception {
@@ -22,21 +25,26 @@ public class Test {
     }
 
     public static void createChartTest() throws NotSavedException, NonExistentArtistException, NonExistentAlbumException {
+        Faker faker = new Faker();
+        Random random = new Random();
         List<ObjectId> albumList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            Artist artist = new Artist("Maroon " + i, "England");
+        for (int i = 0; i < 100; i++) {
+            Artist artist = new Artist(faker.name().fullName(), faker.country().name());
             ArtistController.getInstance().save(artist);
-            Album album = new Album(artist.getId(), "Mambo No. " + i, 2000 + i);
+            Album album = new Album(artist.getId(), faker.funnyName().name(), 1980 + random.nextInt(9));
             AlbumController.getInstance().save(album);
             albumList.add(album.getId());
         }
-        Chart chart = new Chart("Top 10 Mambos", albumList);
+        Chart chart = new Chart("Top 100 funniest songs of the 80's", albumList);
         ChartController.getInstance().save(chart);
     }
 
     public static void rankArtistsTest() {
-        Chart chart = ChartController.getInstance().getByName("Top 10 Mambos");
+        Chart chart = ChartController.getInstance().getByName("Top 100 funniest songs of the 80's");
         List<Artist> topArtists = ChartUtil.getTop(chart);
-        System.out.println("Fifth place: " + topArtists.get(5));
+        System.out.println("Top three funny 80's artists: ");
+        for (int i = 0; i < 3; i++) {
+            System.out.println(topArtists.get(i).getName());
+        }
     }
 }
